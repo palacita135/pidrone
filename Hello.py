d3b080bb7bad23ce78df43d1edc232bef6c7c130
Johar Palacita <3094901+palacita@users.noreply.github.com>
2021-06-30 11:06:33 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,6 +1,6 @@
-from dronekit import connect, VehicleMode, LocationGlobalRelative, GPSInfo, Battery, Version, Capabilities, SystemStatus, Channels, Locations, Vehicle, Parameters, Command, MAVConnection
+from dronekit import connect, VehicleMode, LocationGlobalRelative
 from pymavlink import mavutil
-from __future__ import print_function
+#from __future__ import print_function
 import time
 import sys
 import os
@@ -15,63 +15,63 @@ args = parser.parse_args()
 connection_string = args.connect
 
 # Connect to the Vehicle
-print "Connecting to pidrone2 on: %s" % args.connect
+print ("Connecting to pidrone2 on: %s" % args.connect)
 vehicle = connect(args.connect, baud=57600, wait_ready=True)
 #57600 is the baudrate that you have set in the mission plannar or qgc
 #SERIAL2_PROTOCOL = 2 (the default) to enable MAVLink 2 on the serial port.
 #SERIAL2_BAUD = 57 so the flight controller can communicate with the RPi at 57600 baud.
 #LOG_BACKEND_TYPE = 3 if you are using APSync to stream the dataflash log files to the RPi
 # vehicle is an instance of the Vehicle class
-print "Autopilot Firmware version: %s" % vehicle.version
-print "Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp
-print "Global Location: %s" % vehicle.location.global_frame
-print "Global Location (relative altitude): %s" % vehicle.location.global_relative_frame
-print "Local Location: %s" % vehicle.location.local_frame #NED
-print "Attitude: %s" % vehicle.attitude
-print "Velocity: %s" % vehicle.velocity
-print "GPS: %s" % vehicle.gps_0
-print "Groundspeed: %s" % vehicle.groundspeed
-print "Airspeed: %s" % vehicle.airspeed
-print "Gimbal status: %s" % vehicle.gimbal
-print "Battery: %s" % vehicle.battery
-print "EKF OK?: %s" % vehicle.ekf_ok
-print "Last Heartbeat: %s" % vehicle.last_heartbeat
-print "Rangefinder: %s" % vehicle.rangefinder
-print "Rangefinder distance: %s" % vehicle.rangefinder.distance
-print "Rangefinder voltage: %s" % vehicle.rangefinder.voltage
-print "Heading: %s" % vehicle.heading
-print "Is Armable?: %s" % vehicle.is_armable
-print "System status: %s" % vehicle.system_status.state
-print "Mode: %s" % vehicle.mode.name # settable
-print "Armed: %s" % vehicle.armed # settable
+print ("Autopilot Firmware version: %s" % vehicle.version)
+print ("Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp)
+print ("Global Location: %s" % vehicle.location.global_frame)
+print ("Global Location (relative altitude): %s" % vehicle.location.global_relative_frame)
+print ("Local Location: %s" % vehicle.location.local_frame) #NED
+print ("Attitude: %s" % vehicle.attitude)
+print ("Velocity: %s" % vehicle.velocity)
+print ("GPS: %s" % vehicle.gps_0)
+print ("Groundspeed: %s" % vehicle.groundspeed)
+print ("Airspeed: %s" % vehicle.airspeed)
+print ("Gimbal status: %s" % vehicle.gimbal)
+print ("Battery: %s" % vehicle.battery)
+print ("EKF OK?: %s" % vehicle.ekf_ok)
+print ("Last Heartbeat: %s" % vehicle.last_heartbeat)
+print ("Rangefinder: %s" % vehicle.rangefinder)
+print ("Rangefinder distance: %s" % vehicle.rangefinder.distance)
+print ("Rangefinder voltage: %s" % vehicle.rangefinder.voltage)
+print ("Heading: %s" % vehicle.heading)
+print ("Is Armable?: %s" % vehicle.is_armable)
+print ("System status: %s" % vehicle.system_status.state)
+print ("Mode: %s" % vehicle.mode.name) # settable
+print ("Armed: %s" % vehicle.armed) # settable
 
 # Function to arm and then takeoff to a user specified altitude
 def arm_and_takeoff(aTargetAltitude):
 
-  print "Basic pre-arm checks...DON'T TOUCH!!!"
+  print ("Basic pre-arm checks...DON'T TOUCH!!!")
   # Don't let the user try to arm until autopilot is ready
   while not vehicle.is_armable:
-    print "Waiting for pidrone2 to initialise..."
+    print ("Waiting for pidrone2 to initialise...")
     time.sleep(1)
         
-  print "Arming Motors"
+  print ("Arming Motors")
   # Copter should arm in GUIDED mode
   vehicle.mode    = VehicleMode("GUIDED")
   vehicle.armed   = True
 
   while not vehicle.armed:
-    print " Waiting for arming..."
+    print ("Waiting for arming...")
     time.sleep(1)
 
-  print "Taking off"
+  print ("Taking off")
   vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
 
   # Check that vehicle has reached takeoff altitude
   while True:
-    print "Altitude:", vehicle.location.global_relative_frame.alt 
+    print ("Altitude:", vehicle.location.global_relative_frame.alt)
     #Break and return from function just below target altitude.        
     if vehicle.location.global_relative_frame.alt >= aTargetAltitude*0.95: 
-      print "Reached target altitude"
+      print ("Reached target altitude")
       break
     time.sleep(1)
 
@@ -82,7 +82,7 @@ arm_and_takeoff(10)
 #-- set the default speed
 vehicle.airspeed = 3
 
-print "Take off complete"
+print ("Take off complete")
 
 # Hover for 30 seconds
 time.sleep(30)
@@ -100,7 +100,7 @@ time.sleep(30)
 #print "Return To Home"
 #vehicle.mode = VehicleMode("RTL")
 
-print "Landing Position"
+print ("Landing Position")
 vehicle.mode = VehicleMode("LAND")
 
 # Close vehicle object
