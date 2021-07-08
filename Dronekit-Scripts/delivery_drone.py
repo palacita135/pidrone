49af7b1d5954ce6eb19136c9a93ea674b2e5dfb4
Johar Palacita <3094901+palacita@users.noreply.github.com>
2021-07-08 11:42:25 +0300

    Update delivery_drone.py

--- Dronekit-Scripts/delivery_drone.py
+++ Dronekit-Scripts/delivery_drone.py
@@ -11,8 +11,6 @@ from __future__ import print_function
 from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlobal, Command
 import time
 import math
-#import RPi.GPIO as GPIO
-import wiringpi
 from pymavlink import mavutil
 
 #Set up option parsing to get connection string
@@ -137,9 +135,9 @@ def adds_locations(aLocation): #tampat input point/target lokasi
     cmds.upload()
 
 
-def arm_and_takeoff(TinggiTakeoff):
+def arm_and_takeoff(aTargetAltitude):
     """
-    Arms vehicle and fly to TinggiTakeoff.
+    Arms vehicle and fly to aTargetAltitude.
     """
 
     print("Basic pre-arm checks ... DON'T TOUCH!!!")
@@ -151,6 +149,7 @@ def arm_and_takeoff(TinggiTakeoff):
         
     print("Arming Motors")
     # Copter should arm in GUIDED mode
+    print("Switch mode To GUIDED")
     vehicle.mode = VehicleMode("GUIDED")
     vehicle.armed = True
 
@@ -159,13 +158,13 @@ def arm_and_takeoff(TinggiTakeoff):
         time.sleep(1)
 
     print("Taking Off!!!")
-    vehicle.simple_takeoff(TinggiTakeoff) # Take off to target altitude
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
 
     # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command 
     #  after Vehicle.simple_takeoff will execute immediately).
     while True:
         print("Altitude: ", vehicle.location.global_relative_frame.alt)      
-        if vehicle.location.global_relative_frame.alt>=TinggiTakeoff*0.95: #Trigger just below target alt.
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
             print("Reached Target Altitude")
             break
         time.sleep(1)
