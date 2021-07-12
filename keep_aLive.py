c46fd31771c5897eb9dbe9ca182fb798034b05c7
Johar Palacita <3094901+palacita@users.noreply.github.com>
2021-07-12 20:03:25 +0700

    Create keep_aLive.py

---
+++ keep_aLive.py
@@ -0,0 +1,11 @@
+def do_not_equate_your_life(WithOThers):
+    print("we walk on the same earth")
+    
+    while not but_with():
+        different()
+        #destinies()
+        break
+    time.sleep(1)
+
+if __you__ "human"
+   do_anything_keepYou_alive()
